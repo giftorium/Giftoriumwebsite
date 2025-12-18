440a292ead9771c3410e21a9f50746fc73f742da
unknown <milad.ameri73@gmail.com>
2025-12-18 02:22:46 -0500

    fix: serve media via whitenoise

--- studio_site/wsgi.py
+++ studio_site/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studio_site.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
+
+if settings.MEDIA_ROOT:
+    application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL.lstrip('/'))
